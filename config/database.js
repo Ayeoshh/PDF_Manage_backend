96c8516ee1c3ff42eeace72967c69aa34d495f64
ayeoshh <workingayush08@gmail.com>
2025-03-22 13:29:16 +0530

    postgres added

--- config/database.js
+++ config/database.js
@@ -1,26 +1,48 @@
-const {Sequelize} = require('sequelize');
+// const {Sequelize} = require('sequelize');
+// const dotenv = require('dotenv');
+
+// dotenv.config();
+
+// const sequelize = new Sequelize(
+//     process.env.DB_DATABASE,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//         host: process.env.DB_HOST,
+//         port: process.env.DB_PORT,
+//         dialect: 'postgres',
+//         logging: false,
+//     }
+// );
+
+// (async ()=>{
+//     try{
+//         await sequelize.authenticate();
+//         console.log('PostgresSQL connection established successfully');
+//     }catch(error){
+//         console.log('Unable to connect to postgresSQL:', error);
+//     }
+// })();
+
+// module.exports = sequelize;
+
+
+const { Sequelize } = require('sequelize');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
-const sequelize = new Sequelize(
-    process.env.DB_DATABASE,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: 'postgres',
-        logging: false,
-    }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    logging: false,
+});
 
-(async ()=>{
-    try{
+(async () => {
+    try {
         await sequelize.authenticate();
-        console.log('PostgresSQL connection established successfully');
-    }catch(error){
-        console.log('Unable to connect to postgresSQL:', error);
+        console.log('PostgreSQL connection established successfully');
+    } catch (error) {
+        console.log('Unable to connect to PostgreSQL:', error);
     }
 })();
 
